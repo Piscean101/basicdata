3d2a4ac9311557b86d4cfc46102388660843306e
Deondre <deondre_palmer@hotmail.com>
2023-06-16 16:20:53 -0400

    Home Complete

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
-console.log('test');
-const product = document.getElementsByClassName('product');
-console.log(product);
+
 document.addEventListener("mouseover", e => {
     if (e.target.matches(".product")) {
         /*e.target.style.backgroundColor = 'blue';*/
         e.target.classList.add('color');
-    }
-    for (let i = 0 ; i < product.length + 1 ; i++) {
-        if (e.target.matches(`#product-${i}`)) {
-            /*console.log(e.target.id);*/
-        }
     }
 });
 document.addEventListener("mouseout", e => {
